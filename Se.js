d282779f4ffde1a7eabb322c234a9dabb0030dbe
nunesdiogosilva385-lang <nunesdiogosilva385@gmail.com>
2025-11-13 17:02:24 -0300

    Add service worker for caching assets

---
+++ Se.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "calorify-v2";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css",
+        "/script.js",
+        "/icone-192.png",
+        "/icone-512.png",
+        "/manifest.json",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() => caches.match("/index.html"))
+      );
+    })
+  );
+});
