6271eb03ef987ee1ec122a6f06f1e14d10f35b9b
nunesdiogosilva385-lang <nunesdiogosilva385@gmail.com>
2025-11-14 02:10:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,41 @@
+const CACHE_NAME = "calorify-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/estilos.css",
+  "/script.js",
+  "/icone-192.png",
+  "/icone-512.png",
+  "/logo.png",
+  "/manifest.json"
+];
+
 self.addEventListener("install", (event) => {
-  self.skipWaiting();
   event.waitUntil(
-    caches.open("calorify-cache-v1").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./estilos.css",
-        "./script.js",
-        "./icone-192.png",
-        "./icone-512.png",
-        "./logo.png",
-        "./manifest.json",
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((respostaCache) => {
-      return (
-        respostaCache ||
-        fetch(event.request).catch(() => caches.match("./index.html"))
-      );
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
