573283f4b625eb84081d0aad8b815f685301a29f
nunesdiogosilva385-lang <nunesdiogosilva385@gmail.com>
2025-11-14 01:13:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,31 @@
-// Versão atualizada do Service Worker
-const CACHE_VERSION = "v3-calorify";
-const CACHE_FILES = [
-  "/",
-  "/index.html",
-  "/estilos.css",
-  "/script.js",
-  "/logo.png",
-  "/icone-192.png",
-  "/icone-512.png",
-  "/manifest.json"
-];
-
-// Instalação
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_VERSION).then(cache => {
-      return cache.addAll(CACHE_FILES);
+    caches.open("calorify-cache-v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./estilos.css",
+        "./script.js",
+        "./icone-192.png",
+        "./icone-512.png",
+        "./logo.png",
+        "./manifest.json",
+      ]);
     })
   );
-  self.skipWaiting();
 });
 
-// Ativação (limpa versões antigas)
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_VERSION)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
-// Fetch
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
+    caches.match(event.request).then((respostaCache) => {
       return (
-        resp ||
-        fetch(event.request).catch(() => caches.match("/index.html"))
+        respostaCache ||
+        fetch(event.request).catch(() => caches.match("./index.html"))
       );
     })
   );
